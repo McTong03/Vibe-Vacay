a86710db31ae4222712cbce1c7cc3db192596f87
Claris <ongzy0406@gmail.com>
2026-05-10 14:24:40 +0800

    yay

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -4,8 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="css/admin-dashboard.css">
 </head>
+<style>
+    
+</style>
 <body>
+    <header id="header">
+        <div class="logo-container">
+            <img src="icon/LogoName.png" class="logo" />
+        </div>
+
+        <p class="logo-name">Vibe Vacay</p>
+        <p class="home">Home</p>
+        <p class="destination-management">Destination Management</p>
+        <p class="statistic">Statistic</p>
+        <p class="user-management">User Managememt</p>
+        <p class="logout">Log Out</p>
+
+
+        <div class="profile-box">
+            <p class="profile">Profile</p>
+            <img src="icon/profile1.jpg" class="profile-icon" />
+
+        </div>
+    </header>
+
+    <div class="second-part">
+        <div class="manage-container">
+            <p class="manage">Manage Users</p>
+        </div>
+
+        <div class="admin-container">
+            <p class="admin">Admin / Manage Users</p>
+        </div>
+    </div>  
+
     
 </body>
 </html>
